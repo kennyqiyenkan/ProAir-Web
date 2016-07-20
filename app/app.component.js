f064601b09659b2e6a90b8b30dfdd6044c9e9e22
Kenny Qi-Yen Kan <kanqiyen@gmail.com>
2016-07-20 15:40:54 -0400

    Added parallax scrolling test + navigation bar animation

--- app/app.component.js
+++ app/app.component.js
@@ -12,13 +12,23 @@ var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
     }
+    AppComponent.prototype.myFunction = function () {
+        var x = document.getElementById("navBar");
+        if (x.className === "navbar") {
+            x.className += " responsive";
+        }
+        else {
+            x.className = "navbar";
+        }
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n  <div>\n    <!-- Navigation Bar -->\n    <navbar class=navbar>\n      <ul>\n        <!-- test logo. replace with proair logo\u0095\u0095\u0095 -->\n        <a href=\"/\">\n          <img src=\"http://whitelions.org/wp-content/uploads/2013/08/1-Info-icon-logo-50x50.png\" alt=\"Proair\">\n        </a>\n        <li><a href=\"#Careers\">Careers</a></li>\n        <li><a href=\"#ContactUs\">Contact</a></li>\n        <li><a href=\"#Services\">Services</a></li>\n        <li><a href=\"#About\">About</a></li>\n        <li><a href=\"#Home\">Home</a></li>\n      </ul>\n    </navbar>\n  </div>\n  <div>\n    <!-- Main Body -->\n    <!-- Banner -->\n    <banner class=banner>\n      <img src=\"http://www.9newz.com/wp-content/uploads/2011/02/freedom.jpg\" alt=\"Proair Sdn Bhd\">\n    </banner>\n  </div>\n  ",
+            template: "\n\n  <div>\n    <!-- Navigation Bar -->\n    <navbar class=navbar id=\"navBar\">\n      <ul>\n        <!-- test logo. replace with proair logo\u0095\u0095\u0095 -->\n        <a href=\"/\">\n          <img src=\"http://whitelions.org/wp-content/uploads/2013/08/1-Info-icon-logo-50x50.png\" alt=\"Proair\">\n        </a>\n        <li><a href=\"#Careers\">Careers</a></li>\n        <li><a href=\"#ContactUs\">Contact</a></li>\n        <li><a href=\"#Services\">Services</a></li>\n        <li><a href=\"#About\">About</a></li>\n        <li><a href=\"#Home\">Home</a></li>\n        <li class=icon>\n          <a href=\"javascript:void(0);\" style=\"font-size:15px;\" onclick=\"myFunction()\">\u2630</a>\n        </li>\n      </ul>\n    </navbar>\n\n  </div>\n  <div>\n    <section class=\"module parallax parallax-1\">\n      <div class=\"container\">\n        <h1>Serene</h1>\n      </div>\n      </section>\n\n      <section class=\"module content\">\n      <div class=\"container\">\n        <h2>Lorem Ipsum Dolor</h2>\n        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>\n      </div>\n      </section>\n\n      <section class=\"module parallax parallax-2\">\n      <div class=\"container\">\n        <h1>Rise</h1>\n      </div>\n      </section>\n\n      <section class=\"module content\">\n      <div class=\"container\">\n        <h2>Lorem Ipsum Dolor</h2>\n        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>\n      </div>\n    </section>\n\n    <section class=\"module parallax parallax-3\">\n      <div class=\"container\">\n        <h1>Calm</h1>\n      </div>\n    </section>\n\n    <section class=\"module content\">\n      <div class=\"container\">\n        <h2>Lorem Ipsum Dolor</h2>\n        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>\n      </div>\n    </section>\n  </div>\n  <div>\n\n    <!-- Main Body -->\n    <!-- Banner -->\n    <banner class=banner>\n      <img src=\"http://www.9newz.com/wp-content/uploads/2011/02/freedom.jpg\" alt=\"Proair Sdn Bhd\">\n    </banner>\n  </div>\n\n\n  ",
             styleUrls: [
                 'app/assets/stylesheets/css/navbar.css',
-                'app/assets/stylesheets/css/banner.css'
+                'app/assets/stylesheets/css/banner.css',
+                'app/assets/stylesheets/css/parallaxMain.css'
             ]
         }), 
         __metadata('design:paramtypes', [])
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template:
   `
+
   <div>
     <!-- Navigation Bar -->
-    <navbar class=navbar>
+    <navbar class=navbar id="navBar">
       <ul>
         <!-- test logo. replace with proair logo -->
         <a href="/">
@@ -16,23 +17,78 @@ import { Component } from '@angular/core';
         <li><a href="#Services">Services</a></li>
         <li><a href="#About">About</a></li>
         <li><a href="#Home">Home</a></li>
+        <li class=icon>
+          <a href="javascript:void(0);" style="font-size:15px;" onclick="myFunction()">☰</a>
+        </li>
       </ul>
     </navbar>
+
   </div>
   <div>
+    <section class="module parallax parallax-1">
+      <div class="container">
+        <h1>Serene</h1>
+      </div>
+      </section>
+
+      <section class="module content">
+      <div class="container">
+        <h2>Lorem Ipsum Dolor</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      </div>
+      </section>
+
+      <section class="module parallax parallax-2">
+      <div class="container">
+        <h1>Rise</h1>
+      </div>
+      </section>
+
+      <section class="module content">
+      <div class="container">
+        <h2>Lorem Ipsum Dolor</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      </div>
+    </section>
+
+    <section class="module parallax parallax-3">
+      <div class="container">
+        <h1>Calm</h1>
+      </div>
+    </section>
+
+    <section class="module content">
+      <div class="container">
+        <h2>Lorem Ipsum Dolor</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      </div>
+    </section>
+  </div>
+  <div>
+
     <!-- Main Body -->
     <!-- Banner -->
     <banner class=banner>
       <img src="http://www.9newz.com/wp-content/uploads/2011/02/freedom.jpg" alt="Proair Sdn Bhd">
     </banner>
   </div>
+
+
   `
   ,
   styleUrls: [
         'app/assets/stylesheets/css/navbar.css'
         'app/assets/stylesheets/css/banner.css'
+        'app/assets/stylesheets/css/parallaxMain.css'
     ]
 })
 export class AppComponent {
-
+  myFunction() {
+    var x = document.getElementById("navBar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+  }
 }
