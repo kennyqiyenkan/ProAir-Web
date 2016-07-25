ade6ba9a88085c08673732edc9660a9773805483
Kenny Qi-Yen Kan <kanqiyen@gmail.com>
2016-07-25 18:28:00 -0400

    clean up on css and html. played with angular2. Button not implemented

--- app/app.component.js
+++ app/app.component.js
@@ -11,15 +11,30 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        this.isMobileSizedWidth = true;
     }
+    AppComponent.prototype.ngOnInit = function () {
+        this.isMobileSizedWidth = this.checkMobileSized();
+    };
+    AppComponent.prototype.onResize = function () {
+        this.isMobileSizedWidth = this.checkMobileSized();
+    };
+    AppComponent.prototype.checkMobileSized = function () {
+        if (window.innerWidth <= 680) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n  <div>\n    <!-- Navigation Bar -->\n    <navbar class=navbar id=\"navBar\">\n      <ul>\n        <!-- test logo. replace with proair logo\u0095\u0095\u0095 -->\n        <a href=\"/\">\n          <img src=\"http://whitelions.org/wp-content/uploads/2013/08/1-Info-icon-logo-50x50.png\" alt=\"Proair\">\n        </a>\n        <li><a href=\"#Careers\">Careers</a></li>\n        <li><a href=\"#ContactUs\">Contact</a></li>\n        <li><a href=\"#Services\">Services</a></li>\n        <li><a href=\"#About\">About</a></li>\n        <li class=icon>\n          <a href=\"javascript:void(0);\" style=\"font-size:15px;\" onclick=\"myFunction()\">\u2630</a>\n        </li>\n      </ul>\n    </navbar>\n  </div>\n  <div>\n    <section class=\"module parallax parallax-1\">\n      <div class=\"container\">\n        <h1>Proair</h1>\n      </div>\n    </section>\n\n    <section class=\"module content\">\n      <div class=\"container\">\n        <h2>We Settle All Your Marine HVAC Needs</h2>\n        <p>Company introduction here. Please get contents from a file</p>\n      </div>\n    </section>\n\n    <section class=\"module parallax parallax-2\">\n      <div class=\"container\">\n        <h1>About</h1>\n      </div>\n    </section>\n\n    <section class=\"module content\">\n      <div class=\"container\">\n        <h2>Who We Are</h2>\n        <p>Information about the company here. Please get contents from a file</p>\n      </div>\n    </section>\n\n    <section class=\"module parallax parallax-3\">\n      <div class=\"container\">\n        <h1>Services</h1>\n      </div>\n    </section>\n\n    <section class=\"module content\">\n      <div class=\"container\">\n        <h2>What We Do</h2>\n        <p>List of services here. Please get contents from a file</p>\n      </div>\n    </section>\n\n    <section class=\"module parallax parallax-3\">\n      <div class=\"container\">\n        <h1>Contact</h1>\n      </div>\n    </section>\n\n    <section class=\"module content\">\n      <div class=\"container\">\n        <h2>Where We Are</h2>\n        <p>Contact information here. Please get contents from a file</p>\n      </div>\n    </section>\n\n    <section class=\"module parallax parallax-3\">\n      <div class=\"container\">\n        <h1>Careers</h1>\n      </div>\n    </section>\n\n    <section class=\"module content\">\n      <div class=\"container\">\n        <h2>Want To Work Here?</h2>\n        <p>List of openings here. Please get contents from a file</p>\n      </div>\n    </section>\n  </div>\n  <div>\n\n    <!-- Main Body -->\n    <!-- Banner\n    <banner class=banner>\n      <img src=\"http://www.9newz.com/wp-content/uploads/2011/02/freedom.jpg\" alt=\"Proair Sdn Bhd\">\n    </banner>\n    -->\n  </div>\n\n\n  ",
+            template: "\n  <div (window:resize)=\"onResize($event)\"> <!-- Navigation Bar -->\n    <navbar class=navbar id=\"navBar\">\n      <ul>\n        <!-- test logo. replace with proair logo\u0095\u0095\u0095 -->\n        <a href=\"/\">\n          <img src=\"http://whitelions.org/wp-content/uploads/2013/08/1-Info-icon-logo-50x50.png\" alt=\"Proair\">\n        </a>\n        <li *ngIf=\"!isMobileSizedWidth\"><a href=\"#Careers\">Careers</a></li>\n        <li *ngIf=\"!isMobileSizedWidth\"><a href=\"#ContactUs\">Contact</a></li>\n        <li *ngIf=\"!isMobileSizedWidth\"><a href=\"#Services\">Services</a></li>\n        <li *ngIf=\"!isMobileSizedWidth\"><a href=\"#About\">About</a></li>\n        <li *ngIf=\"isMobileSizedWidth\"><button>\u2630</button></li>\n      </ul>\n    </navbar>\n  </div>\n\n  <div style=\"margin-top:-20px;\">\n    <section class=\"module parallax parallax-1\" id=\"sectionHome\">\n      <div class=\"container\">\n        <h1>Proair</h1>\n      </div>\n    </section>\n\n    <section class=\"module content\">\n      <div class=\"container\">\n        <h2>We Settle All Your Marine HVAC Needs</h2>\n        <p>Company introduction here. Please get contents from a file</p>\n      </div>\n    </section>\n\n    <section class=\"module parallax parallax-2\" id=\"sectionAbout\">\n      <div class=\"container\">\n        <h1>About</h1>\n      </div>\n    </section>\n\n    <section class=\"module content\">\n      <div class=\"container\">\n        <h2>Who We Are</h2>\n        <p>Information about the company here. Please get contents from a file</p>\n      </div>\n    </section>\n\n    <section class=\"module parallax parallax-3\" id=\"sectionServices\">\n      <div class=\"container\">\n        <h1>Services</h1>\n      </div>\n    </section>\n\n    <section class=\"module content\">\n      <div class=\"container\">\n        <h2>What We Do</h2>\n        <p>List of services here. Please get contents from a file</p>\n      </div>\n    </section>\n\n    <section class=\"module parallax parallax-3\" id=\"sectionContact\">\n      <div class=\"container\">\n        <h1>Contact</h1>\n      </div>\n    </section>\n\n    <section class=\"module content\">\n      <div class=\"container\">\n        <h2>Where We Are</h2>\n        <p>Contact information here. Please get contents from a file</p>\n      </div>\n    </section>\n\n    <section class=\"module parallax parallax-3\" id=\"sectionCareers\">\n      <div class=\"container\">\n        <h1>Careers</h1>\n      </div>\n    </section>\n\n    <section class=\"module content\">\n      <div class=\"container\">\n        <h2>Want To Work Here?</h2>\n        <p>List of openings here. Please get contents from a file</p>\n      </div>\n    </section>\n  </div>\n  <div>\n    <footer class=\"footer\">\n      <p>Proair Sdn. Bhd.</p>\n    </footer>\n  </div>\n  ",
             styleUrls: [
                 'app/assets/stylesheets/css/navbar.css',
-                'app/assets/stylesheets/css/banner.css',
-                'app/assets/stylesheets/css/parallaxMain.css'
+                'app/assets/stylesheets/css/parallaxMain.css',
+                'app/assets/stylesheets/css/footer.css'
             ]
         }), 
         __metadata('design:paramtypes', [])
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,26 +3,24 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template:
   `
-  <div>
-    <!-- Navigation Bar -->
+  <div (window:resize)="onResize($event)"> <!-- Navigation Bar -->
     <navbar class=navbar id="navBar">
       <ul>
         <!-- test logo. replace with proair logo -->
         <a href="/">
           <img src="http://whitelions.org/wp-content/uploads/2013/08/1-Info-icon-logo-50x50.png" alt="Proair">
         </a>
-        <li><a href="#Careers">Careers</a></li>
-        <li><a href="#ContactUs">Contact</a></li>
-        <li><a href="#Services">Services</a></li>
-        <li><a href="#About">About</a></li>
-        <li class=icon>
-          <a href="javascript:void(0);" style="font-size:15px;" onclick="myFunction()">☰</a>
-        </li>
+        <li *ngIf="!isMobileSizedWidth"><a href="#Careers">Careers</a></li>
+        <li *ngIf="!isMobileSizedWidth"><a href="#ContactUs">Contact</a></li>
+        <li *ngIf="!isMobileSizedWidth"><a href="#Services">Services</a></li>
+        <li *ngIf="!isMobileSizedWidth"><a href="#About">About</a></li>
+        <li *ngIf="isMobileSizedWidth"><button>☰</button></li>
       </ul>
     </navbar>
   </div>
-  <div>
-    <section class="module parallax parallax-1">
+
+  <div style="margin-top:-20px;">
+    <section class="module parallax parallax-1" id="sectionHome">
       <div class="container">
         <h1>Proair</h1>
       </div>
@@ -35,7 +33,7 @@ import { Component } from '@angular/core';
       </div>
     </section>
 
-    <section class="module parallax parallax-2">
+    <section class="module parallax parallax-2" id="sectionAbout">
       <div class="container">
         <h1>About</h1>
       </div>
@@ -48,7 +46,7 @@ import { Component } from '@angular/core';
       </div>
     </section>
 
-    <section class="module parallax parallax-3">
+    <section class="module parallax parallax-3" id="sectionServices">
       <div class="container">
         <h1>Services</h1>
       </div>
@@ -61,7 +59,7 @@ import { Component } from '@angular/core';
       </div>
     </section>
 
-    <section class="module parallax parallax-3">
+    <section class="module parallax parallax-3" id="sectionContact">
       <div class="container">
         <h1>Contact</h1>
       </div>
@@ -74,7 +72,7 @@ import { Component } from '@angular/core';
       </div>
     </section>
 
-    <section class="module parallax parallax-3">
+    <section class="module parallax parallax-3" id="sectionCareers">
       <div class="container">
         <h1>Careers</h1>
       </div>
@@ -88,24 +86,34 @@ import { Component } from '@angular/core';
     </section>
   </div>
   <div>
-
-    <!-- Main Body -->
-    <!-- Banner
-    <banner class=banner>
-      <img src="http://www.9newz.com/wp-content/uploads/2011/02/freedom.jpg" alt="Proair Sdn Bhd">
-    </banner>
-    -->
+    <footer class="footer">
+      <p>Proair Sdn. Bhd.</p>
+    </footer>
   </div>
-
-
   `
   ,
   styleUrls: [
         'app/assets/stylesheets/css/navbar.css'
-        'app/assets/stylesheets/css/banner.css'
         'app/assets/stylesheets/css/parallaxMain.css'
+        'app/assets/stylesheets/css/footer.css'
     ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  private isMobileSizedWidth = true;
 
+  ngOnInit()
+  {
+    this.isMobileSizedWidth = this.checkMobileSized();
+  }
+
+  onResize()
+  {
+    this.isMobileSizedWidth = this.checkMobileSized();
+  }
+
+  checkMobileSized()
+  {
+    if(window.innerWidth <= 680) { return true; }
+    else{ return false; }
+  }
 }
